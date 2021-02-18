f73888cdcd235e25a5527e2983176e62988f1c04
Zane Hitchcox <zwhitchcox@gmail.com>
2021-02-18 16:42:33 -0500

    await stop poll
    Change-type: patch

--- src/index.ts
+++ src/index.ts
@@ -160,6 +160,11 @@ const initializeRNDIS = (device: usb.Device): usb.InEndpoint => {
 	return iEndpoint;
 };
 
+const stopPoll = async (inEndpoint: usb.InEndpoint) => (
+	new Promise<void>(res => {
+		inEndpoint.stopPoll(res)
+	})
+)
 export class UsbBBbootScanner extends EventEmitter {
 	private usbBBbootDevices = new Map<string, UsbBBbootDevice>();
 	private boundAttachDevice: (device: usb.Device) => Promise<void>;
@@ -331,9 +336,9 @@ export class UsbBBbootScanner extends EventEmitter {
 							await this.transfer(device, outEndpoint, tftpBuff);
 						} else {
 							if (platform === 'win32' || platform === 'darwin') {
-								rndisInEndpoint.stopPoll();
+								await stopPoll(rndisInEndpoint);
 							}
-							inEndpoint.stopPoll();
+							await stopPoll(inEndpoint);
 							this.closeDevice(device, 0);
 						}
 					}
